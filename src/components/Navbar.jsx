a07da4a83463fa249465c61e3857cdaeca62bd9f
jooho2055 <jooho2055@gmail.com>
2024-01-05 01:37:45 -0800

    created a nav bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiShoppingBag } from 'react-icons/fi';
+import { BsFillPencilFill } from 'react-icons/bs';
+import jshopLogo from '../assets/svg/jshopLogo.svg';
+
+export default function Navbar() {
+	return (
+		<header>
+			<Link to={'/'}>
+				<img src={jshopLogo} alt='Jshop Logo' />
+				<h1>Jshop</h1>
+			</Link>
+			<nav>
+				<Link to={'/products'}>Products</Link>
+				<Link to={'/carts'}>Carts</Link>
+				<Link to={'/products/new'}>
+					<BsFillPencilFill />
+				</Link>
+				<button>Login</button>
+			</nav>
+		</header>
+	);
+}
