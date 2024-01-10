55ee378bc0d9c550a2fdf563c051f5e81487c708
jooho2055 <jooho2055@gmail.com>
2024-01-10 02:03:11 -0800

    implemented add new product form

--- src/pages/NewProducts.jsx
+++ src/pages/NewProducts.jsx
@@ -1,5 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../components/ui/Button';
 
 export default function NewProducts() {
-	return <div>this is new products!</div>;
+	const [product, setProduct] = useState({});
+	const [file, setFile] = useState();
+
+	const handleChange = (e) => {
+		const { name, value, files } = e.target;
+		if (name === 'file') {
+			setFile(files && files[0]);
+			return;
+		}
+		setProduct((product) => ({ ...product, [name]: value })); // 입력폼에서 변경이 발생한 이름의 키의 value 를 할당해줌
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		// add a photo of a product to cloudinary and get url
+		// add the new product in firebase
+	};
+
+	return (
+		<section>
+			{file && <img src={URL.createObjectURL(file)} alt='local file' />}
+			<form onSubmit={handleSubmit}>
+				<input
+					type='file' //
+					accept='image/*'
+					name='file'
+					required
+					onChange={handleChange}
+				/>
+				<input
+					type='text'
+					name='title'
+					value={product.title ?? ''}
+					placeholder='Product Name'
+					required
+					onChange={handleChange}
+				/>
+				<input
+					type='number'
+					name='price'
+					value={product.price ?? ''}
+					placeholder='Price'
+					required
+					onChange={handleChange}
+				/>
+				<input
+					type='text'
+					name='category'
+					value={product.category ?? ''}
+					placeholder='Category'
+					required
+					onChange={handleChange}
+				/>
+				<input
+					type='text'
+					name='description'
+					value={product.description ?? ''}
+					placeholder='Description'
+					required
+					onChange={handleChange}
+				/>
+				<input
+					type='text'
+					name='options'
+					value={product.options ?? ''}
+					placeholder='Options (separated by commas)'
+					required
+					onChange={handleChange}
+				/>
+				<Button text={'Add Product'} />
+			</form>
+		</section>
+	);
 }
